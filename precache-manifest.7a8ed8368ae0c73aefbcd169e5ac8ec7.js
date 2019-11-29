4d88f958c9efd7e72d750400816b7711d4e17b4f
Orie Steele <orie@ohs.io>
2019-11-29 15:53:41 -0600

    Updates

---
+++ precache-manifest.7a8ed8368ae0c73aefbcd169e5ac8ec7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74d51467c92a0184382d03be59568a06",
+    "url": "/vc-greeting-card/index.html"
+  },
+  {
+    "revision": "332181922d435d0ecbd4",
+    "url": "/vc-greeting-card/static/css/main.e90be185.chunk.css"
+  },
+  {
+    "revision": "1fa62c4213533bf8e874",
+    "url": "/vc-greeting-card/static/js/2.1ab0de30.chunk.js"
+  },
+  {
+    "revision": "332181922d435d0ecbd4",
+    "url": "/vc-greeting-card/static/js/main.74eb009d.chunk.js"
+  },
+  {
+    "revision": "1dae96b7c06b375473ed",
+    "url": "/vc-greeting-card/static/js/runtime-main.f0d95676.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vc-greeting-card/precache-manifest.f1df15ce78f668182bd6db336e883185.js"
+  "/vc-greeting-card/precache-manifest.7a8ed8368ae0c73aefbcd169e5ac8ec7.js"
 );
 
 self.addEventListener('message', (event) => {
