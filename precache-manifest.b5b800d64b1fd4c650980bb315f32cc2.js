01188a2d8fb44ca141e916eb0f4b57fc05dfb38f
Orie Steele <orie@ohs.io>
2019-11-29 13:46:08 -0600

    Updates

---
+++ precache-manifest.b5b800d64b1fd4c650980bb315f32cc2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b36a00b35ab572638792188cf86eb6a",
+    "url": "/vc-greeting-card/index.html"
+  },
+  {
+    "revision": "5a7502b0d6b2fa781030",
+    "url": "/vc-greeting-card/static/css/main.012ff5c3.chunk.css"
+  },
+  {
+    "revision": "a38063fa074a3928bc83",
+    "url": "/vc-greeting-card/static/js/2.208c83f8.chunk.js"
+  },
+  {
+    "revision": "5a7502b0d6b2fa781030",
+    "url": "/vc-greeting-card/static/js/main.a919f868.chunk.js"
+  },
+  {
+    "revision": "1dae96b7c06b375473ed",
+    "url": "/vc-greeting-card/static/js/runtime-main.f0d95676.js"
+  }
+]);
